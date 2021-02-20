42c21a812f251458c4efb8b861e0ee0fee32f585
Igor Taranenko <igorgronthebest@gmail.com>
2021-02-20 13:48:46 +0200

    Created 2 layouts, created top navbar, added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '', component: AuthLayoutComponent, children: [
+            {path: '', redirectTo: '/login', pathMatch: 'full'},
+            {path: 'login', component: LoginPageComponent},
+            {path: 'sign-up', component: SignUpPageComponent}
+        ]
+    },
+    {
+        path: '', component: SiteLayoutComponent, children: [
+
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginPageComponent,
+        AuthLayoutComponent,
+        SiteLayoutComponent,
+        SignUpPageComponent
     ],
     imports: [
         BrowserModule,
